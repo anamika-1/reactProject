1d3725f6a160ec52c4e17965e55426c18f8c937a
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2024-04-29 08:20:20 +0530

    reactRouter

--- project_4/reactrouter/src/App.jsx
+++ project_4/reactrouter/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <>
-      <h1 className='bg-green-600 p-4'>Radhe Radhe</h1>
+      
     </>
   )
 }
