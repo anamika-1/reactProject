c06b51e25c8467a8d2167858a89cff1a6edf9e14
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-04-29 22:25:53 +0530

    Fixed typos of Input component and useCurrencyInfo

--- project_3/currencyConverter/src/components/InputBox.jsx
+++ project_3/currencyConverter/src/components/InputBox.jsx
@@ -6,9 +6,9 @@ function InputBox({
     onAmountChange,
     onCurrencyChange,
     currencyOptions = [],
-    selectCurrency = "usd",
-    amountDisable = false,
-    currencyDisable = false,
+    selectedCurrency = "usd",
+    amountDisabled = false,
+    currencyDisabled = false,
     className = "",
 }) {
    const amountInputId = useId()
@@ -24,18 +24,18 @@ function InputBox({
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
-                    disabled={amountDisable}
+                    disabled={amountDisabled}
                     value={amount}
-                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                    onChange={(e) => onAmountChange && onAmountChange(Number((e.target.value)))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    value={selectCurrency}
-                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-                    disabled={currencyDisable}
+                    value={selectedCurrency}
+                    onChange={(e) => {onCurrencyChange && onCurrencyChange(e.target.value)}}
+                    disabled={currencyDisabled}
                 >
                     
                         {currencyOptions.map((currency) => (
