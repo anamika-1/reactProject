badb64366b51c355cec707083fc2de908633b93a
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2024-04-30 17:51:16 +0530

    context

--- project_4/reactrouter/src/components/Github/Github.jsx
+++ project_4/reactrouter/src/components/Github/Github.jsx
@@ -18,7 +18,9 @@ function Github() {
     // }, [])
 
     return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}</div>
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>         
+         Github followers: {data.followers}
+         </div>
     )
 }
 
