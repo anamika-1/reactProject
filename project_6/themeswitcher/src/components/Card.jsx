4c3ea73cbb719ce6f137dff28a22d0d1993b1fd0
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2024-04-30 22:09:30 +0530

    themeswitcher

---
+++ project_6/themeswitcher/src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <div className="w-full h-40  bg-white border flex justify-center items-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <h1 className=' text-cyan-500  font-bold '>THEME SWITCHER</h1>
+        </div>
+    );
+}
